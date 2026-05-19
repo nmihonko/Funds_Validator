47c15b87119f88bb596c92ae41e985f95473c902
Nazar Mihonko <migonkonazar@gmail.com>
2026-05-19 18:33:06 +0300

    Fix check in the method

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    static double balance;
+
 
     public static void main(String[] args) {
-        balance = 1500;
-        double amount = getWithdrawalAmount();
+        double balance = 1500;
 
         try {
+            double amount = getWithdrawalAmount(balance);
             validateWithdrawalAmount(balance, amount);
         } catch (FundsException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
     }
 
-
-    private static double getWithdrawalAmount() {
+    private static double getWithdrawalAmount(double balance) throws  FundsException{
         System.out.printf("Balance is USD %.2f.%n" + "Enter purchase amount, USD: ", balance);
         Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextDouble()) {
+            throw new FundsException("Withdrawal amount cannot be a text");
+        }
         double amount = scanner.nextDouble();
-
+        if (amount <= 0) {
+            throw new FundsException("Write a valid Withdrawal amount that greater than 0.");
+        }
         return amount;
     }
 
@@ -30,13 +34,13 @@ public class Main {
     private static void validateWithdrawalAmount(double balance, double withdrawal) throws FundsException {
 
         if (withdrawal > balance) {
-            StringBuilder errorMessage = new StringBuilder().append("Insufficient funds! ").append("Need: $").append(withdrawal).append(", ").append("Available: $").append(balance);
-            throw new FundsException(errorMessage.toString());
-        } else {
-            double newBalance = getRemainingBalance(balance, withdrawal);
-            StringBuilder successMessage = new StringBuilder().append("Funds are OK. Purchase paid.\n").append("Balance is USD ").append(String.format("%.2f", newBalance));
-            System.out.printf(successMessage.toString());
+            String errorMessage = String.format("Insufficient funds! Need: $%.2f, Available: $%.2f", withdrawal, balance);
+            throw new FundsException(errorMessage);
         }
+
+        double newBalance = getRemainingBalance(balance, withdrawal);
+
+        System.out.printf("Funds are OK. Purchase paid.%nBalance is USD %.2f", newBalance);
     }
 
     private static double getRemainingBalance(double balance, double withdrawal) {
